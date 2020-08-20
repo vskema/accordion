1c50dd0ec53ab4c722f14b0159ba58c624cfcd1c
Vaidotas <skema.vaidotas@gmail.com>
2020-08-20 22:06:59 +0300

    finished accordion widget

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Accordion = ({items}) => {
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const onTitleClick = (index) => {
-        console.log('Title clicked', index);
+        setActiveIndex(index)
     }
 
     const renderedItems = items.map((item, index) => {
+
+        const active = index === activeIndex ? 'active' : '';
+
         return (
         <React.Fragment key={item.title}>
-                    <div
-                        className="title active"
-                        onClick={() => onTitleClick(index)}
-                    >
+                    <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
                         <i className="dropdown icon"></i>
                         {item.title}
                     </div>
-                    <div className="content active">
+                    <div className={`content ${active}`}>
                         <p>{item.content}</p>
                     </div>
          </React.Fragment>
