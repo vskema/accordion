5bd66a642511792a53b17971faafa82e4815385f
Vaidotas <skema.vaidotas@gmail.com>
2020-08-21 22:18:32 +0300

    add articles list to the page

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,6 +24,17 @@ const Search = () => {
 
     }, [term]);
 
+    const renderedResults = results.map((result) => {
+        return <div key={result.pageid} className='item'>
+                    <div className='content'>
+                        <div className='header'>
+                            {result.title}
+                        </div>
+                        {result.snippet}
+                    </div>
+                </div>
+    })
+
     return (
         <div>
             <div className='ui form'>
@@ -35,6 +46,9 @@ const Search = () => {
                         className='input'
                     />
                 </div>
+            </div>
+            <div className='ui celled list'>
+                {renderedResults}
             </div>
         </div>
     );
