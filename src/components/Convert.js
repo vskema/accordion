0c6170dc46024845b6d45b3140f9e0899c240fea
Vaidotas <skema.vaidotas@gmail.com>
2020-08-28 22:07:13 +0300

    convert component useEffect with axios

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,20 +2,31 @@ import React, {useState, useEffect}  from "react";
 import axios from "axios";
 
 const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState('');
 
     useEffect(() => {
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params: {
-                q: text,
-                target: language.value,
-                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }
-        })
-    }, [language, text]);
+
+            const doTranslation = async () => {
+                const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                    params: {
+                        q: text,
+                        target: language.value,
+                        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                    }
+                });
+
+                setTranslated(data.data.translations[0].translatedText);
+            };
+            doTranslation()
+        }, [language, text]
+    );
+
 
 
     return (
-        <div></div>
+        <div>
+            <h1 className='ui header'>{translated}</h1>
+        </div>
     )
 
 }
