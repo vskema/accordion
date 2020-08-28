f919484fbf8d6dde48e86c559bed6da5b8ce2cfe
Vaidotas <skema.vaidotas@gmail.com>
2020-08-28 23:35:08 +0300

    add Route component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
 
 const items = [
     {
@@ -32,13 +33,58 @@ const options = [
         label: 'A Shade of Blue',
         value: 'blue'
     }
-]
+];
+
+const showAccordion = () => {
+    if(window.location.pathname === '/') {
+        return <Accordion
+            items={items}
+        />
+    }
+}
+
+const showList= () => {
+    if(window.location.pathname === '/list'){
+        return <Search />
+    }
+}
+
+const showDropdown= () => {
+    if(window.location.pathname === '/dropdown'){
+        return <Dropdown
+
+        />
+    }
+}
+
+const showTranslate= () => {
+    if(window.location.pathname === '/translate'){
+        return <Translate />
+    }
+}
 
 export default  () => {
-
+const [selected, setSelected] = useState(options[0])
     return (
         <div>
-            <Translate />
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    options = {options}
+                    label="Select a color"
+                    selected = {selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
+
         </div>
     )
 }
